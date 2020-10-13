ce1f513f83d6d24ae3522e6ba37015190fe9d3d6
João Batista <j.batista.romao@gmail.com>
2020-10-13 17:23:52 -0300

    First Commit

---
+++ SmartHome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartHome
+//
+//  Created by Joao Batista on 13/10/20.
+//  Copyright © 2020 Joao Batista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
